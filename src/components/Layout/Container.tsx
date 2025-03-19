bce2a72d0e016d7d61c00de48fe2012f33d6205e
Курило Олег Игоревич <kurilo.o.i@sberbank.ru>
2025-03-19 16:13:18 +0300

    новая сборка на vite; новый бэкграунд

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -1,11 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import backgroundSrc from "../../assets/background.svg?react";
 
-export const Container = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100vh',
-  width: '100%',
-  background: 'linear-gradient(120deg, #c4e6c3 0%, #d7c7e9 100%)',
-  position: 'relative',
-  overflow: 'hidden'
+export const Container = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  height: "100vh",
+  width: "100%",
+  backgroundImage: `url(${backgroundSrc})`,
+  backgroundSize: "cover",
+  position: "relative",
+  overflow: "hidden",
 });
